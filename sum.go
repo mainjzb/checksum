546b39f98402d8936a116008f9bdd298aae5145f
mainjzb <mainjzb@live.com>
2023-11-07 17:39:28 +0800

    更新自带校验方法

--- sum.go
+++ sum.go
@@ -1,19 +1,28 @@
 package utils
 
 // BBC 异或校验和
-func BBC(data []byte) uint8 {
+func BBC(data []byte) []byte {
 	var result uint8 = 0
 	for _, d := range data {
 		result ^= d
 	}
-	return result
+	return []byte{result}
 }
 
-// Sum2 累加校验和
-func Sum2(data []byte) uint16 {
+// Sum2BE 累加校验和 大段
+func Sum2BE(data []byte) []byte {
 	var sum uint16
 	for _, c := range data {
 		sum += uint16(c)
 	}
-	return sum
+	return []byte{uint8(sum / 256), uint8(sum % 256)}
+}
+
+// Sum2LE 累加校验和 小段
+func Sum2LE(data []byte) []byte {
+	var sum uint16
+	for _, c := range data {
+		sum += uint16(c)
+	}
+	return []byte{uint8(sum % 256), uint8(sum / 256)}
 }
